3910400e6153bff9b73fa884e1d29acf1ec033c7
Alex Kim <312akim@gmail.com>
2020-03-28 11:46:31 -0500

    Week 4 Assignment

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function() { //Ready Method (When doc is ready, run script)
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        }
+        else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        };
+    });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
